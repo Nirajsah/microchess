e5ed814aa9d2abd70213338f77da28c9720852cf
Nirajsah <nirajsahy4aj@gmail.com>
2024-07-29 20:29:53 +0530

    feat: Add move_piece method to ChessBoard struct
    This commit adds the move_piece method to the ChessBoard struct. The method allows for moving a chess piece from one square to another by updating the corresponding bitboard. This is a crucial functionality for implementing the chess game logic and handling GraphQL mutations.

--- chess/src/lib.rs
+++ chess/src/lib.rs
@@ -1,3 +1,4 @@
+#![allow(non_snake_case)]
 use async_graphql::{Request, Response, SimpleObject};
 use linera_sdk::base::{ContractAbi, ServiceAbi};
 use serde::{Deserialize, Serialize};
@@ -15,11 +16,61 @@ impl ServiceAbi for ChessAbi {
 }
 
 #[derive(Debug, Deserialize, Serialize, Clone, GraphQLMutationRoot)]
+#[serde(rename_all = "camelCase")]
 pub enum Operation {
     NewGame,
+    MakeMove { from: String, to: String },
 }
 
-#[derive(Clone, Debug, Deserialize, Serialize, SimpleObject)]
-pub struct Board {
-    pub board: Vec<String>,
+pub type Bitboard = u64;
+
+#[derive(Clone, Default, Debug, Deserialize, Serialize, SimpleObject)]
+pub struct ChessBoard {
+    pub wP: Bitboard,
+    pub wN: Bitboard,
+    pub wB: Bitboard,
+    pub wR: Bitboard,
+    pub wQ: Bitboard,
+    pub wK: Bitboard,
+    pub bP: Bitboard,
+    pub bN: Bitboard,
+    pub bB: Bitboard,
+    pub bR: Bitboard,
+    pub bQ: Bitboard,
+    pub bK: Bitboard,
+}
+
+#[derive(Clone, Default, Deserialize, Serialize, SimpleObject)]
+pub struct Game {
+    pub board: ChessBoard,
+}
+
+impl ChessBoard {
+    pub fn new() -> Self {
+        ChessBoard {
+            wP: 0x000000000000FF00,
+            wN: 0x0000000000000042,
+            wB: 0x0000000000000024,
+            wR: 0x0000000000000081,
+            wQ: 0x0000000000000008,
+            wK: 0x0000000000000010,
+            bP: 0x00FF000000000000,
+            bN: 0x4200000000000000,
+            bB: 0x2400000000000000,
+            bR: 0x8100000000000000,
+            bQ: 0x0800000000000000,
+            bK: 0x1000000000000000,
+        }
+    }
+
+    pub fn move_piece(&mut self, from_square: u64, to_square: u64, bitboard: &mut Bitboard) {
+        let from_bit = 1u64 << from_square as u32;
+        let to_bit = 1u64 << to_square as u32;
+
+        // Clear the bit at the original position
+        *bitboard &= !from_bit;
+
+        // Set the bit at the new position
+        *bitboard |= to_bit;
+    }
 }
