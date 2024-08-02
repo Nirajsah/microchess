dad5a8a0c0a66061dccc4c0823a58f70f45385f3
Nirajsah <nirajsahy4aj@gmail.com>
2024-08-02 21:09:05 +0530

    ```text refactor: Update Chess struct to use ChessBoard instead of Game
    This commit updates the Chess struct in the state.rs file to use the ChessBoard struct instead of the Game struct. The ChessBoard struct represents the state of the chess game, and this change ensures consistency and clarity in the codebase. It also aligns with the recent changes made to the ChessService and ChessContract modules.

--- chess/src/state.rs
+++ chess/src/state.rs
@@ -1,18 +1,18 @@
 extern crate chess_rs;
-use chess::{ChessBoard, Game};
+use chess::ChessBoard;
 use linera_sdk::views::{linera_views, RegisterView, RootView, ViewStorageContext};
 
 #[derive(RootView, async_graphql::SimpleObject)]
 #[view(context = "ViewStorageContext")]
 
 pub struct Chess {
-    pub board: RegisterView<Game>,
+    pub board: RegisterView<ChessBoard>,
 }
 
 #[allow(dead_code)]
 impl Chess {
     pub fn new(&mut self) {
         let board: ChessBoard = ChessBoard::new();
-        self.board.set(Game { board });
+        self.board.set(board);
     }
 }
