779e2b38c7cd2842328b56e2eca43996c89cfa77
Nirajsah <nirajsahy4aj@gmail.com>
2024-08-05 17:21:38 +0530

    refactor: Update knight_moves to knight_attacks in Game struct
    This commit updates the `knight_moves` field in the `Game` struct to `knight_attacks`. The field now represents the pre-computed knight attacks for each square on the chessboard.

--- chess/src/lib.rs
+++ chess/src/lib.rs
@@ -33,6 +33,8 @@ pub enum Color {
 
 const NOT_A_FILE: Bitboard = 0xFEFEFEFEFEFEFEFE;
 const NOT_H_FILE: Bitboard = 0x7F7F7F7F7F7F7F7F;
+const NOT_HG_FILE: Bitboard = 0x3F3F3F3F3F3F3F3F;
+const NOT_AB_FILE: Bitboard = 0xFCFCFCFCFCFCFCFC;
 
 /// A struct to represent a chess piece
 #[derive(Ord, PartialOrd, Eq, PartialEq, Debug, Clone, Copy, Serialize, Deserialize)]
@@ -88,8 +90,8 @@ pub struct ChessBoard {
 pub struct Game {
     pub wp_moves: Vec<Bitboard>,
     pub bp_moves: Vec<Bitboard>,
-    pub knight_moves: Vec<Vec<Bitboard>>,
-    pub king_moves: Vec<Vec<Bitboard>>,
+    pub knight_attacks: Vec<Bitboard>,
+    pub king_moves: Vec<Bitboard>,
 }
 
 impl Game {
@@ -98,7 +100,7 @@ impl Game {
         Game {
             wp_moves: Self::computed_pawn_moves(&Color::White),
             bp_moves: Self::computed_pawn_moves(&Color::Black),
-            knight_moves: Self::computed_knight_moves(),
+            knight_attacks: Self::computed_knight_attacks(),
             king_moves: Self::computed_king_moves(),
         }
     }
@@ -113,14 +115,22 @@ impl Game {
         pawn_moves
     }
 
-    /// A function to compute knight moves
-    pub fn computed_knight_moves() -> Vec<Vec<Bitboard>> {
-        let moves = vec![vec![]];
+    /// A function to compute knight attacks
+    pub fn computed_knight_attacks() -> Vec<Bitboard> {
+        let mut moves = Vec::new();
+        for i in 0..64 {
+            let boards = Self::attacks_knight_moves(i);
+            moves.push(boards);
+        }
         moves
     }
     /// A function to compute king moves
-    pub fn computed_king_moves() -> Vec<Vec<Bitboard>> {
-        let moves = vec![vec![]];
+    pub fn computed_king_moves() -> Vec<Bitboard> {
+        let mut moves = Vec::new();
+        for i in 0..64 {
+            let boards = Self::attacks_king_moves(i);
+            moves.push(boards);
+        }
         moves
     }
 
@@ -167,6 +177,44 @@ impl Game {
                 attacks |= (board >> 7) & NOT_A_FILE; // Black pawns attack down-right
             }
         }
+        attacks
+    }
+
+    /// possible knight attacks
+    pub fn attacks_knight_moves(square: u8) -> Bitboard {
+        let mut attacks = 0u64;
+        let mut board: Bitboard = 0u64;
+
+        board |= 1u64 << square as u64; // Set the bit at the square
+
+        attacks |= (board >> 17) & NOT_H_FILE; // Knight attacks up-right
+        attacks |= (board >> 15) & NOT_A_FILE; // Knight attacks up-left
+        attacks |= (board >> 10) & NOT_HG_FILE; // Knight attacks right-up
+        attacks |= (board >> 6) & NOT_AB_FILE; // Knight attacks left-up
+        attacks |= (board << 17) & NOT_A_FILE; // Knight attacks down-left
+        attacks |= (board << 15) & NOT_H_FILE; // Knight attacks down-right
+        attacks |= (board << 10) & NOT_AB_FILE; // Knight attacks left-down
+        attacks |= (board << 6) & NOT_HG_FILE; // Knight attacks right-down
+
+        attacks
+    }
+
+    /// possible king moves
+    pub fn attacks_king_moves(square: u8) -> Bitboard {
+        let mut attacks = 0u64;
+        let mut board: Bitboard = 0u64;
+
+        board |= 1u64 << square as u64; // Set the bit at the square
+
+        attacks |= (board >> 9) & NOT_H_FILE; // King attacks up-right
+        attacks |= board >> 8; // King attacks up
+        attacks |= (board >> 7) & NOT_A_FILE; // King attacks up-left
+        attacks |= (board >> 1) & NOT_H_FILE; // King attacks right
+        attacks |= (board << 1) & NOT_A_FILE; // King attacks left
+        attacks |= (board << 7) & NOT_H_FILE; // King attacks
+        attacks |= board << 8; // King attacks down
+        attacks |= (board << 9) & NOT_A_FILE; // King
+
         attacks
     }
 }
