18fb063272eeeac5f86e3e73e5eec4ab56eb6a1d
Nirajsah <nirajsahy4aj@gmail.com>
2024-08-12 14:08:54 +0530

    refactor: Update ChessService instantiate method to set owners and instantiate board

--- chess/src/state.rs
+++ chess/src/state.rs
@@ -1,18 +1,16 @@
 extern crate chess_rs;
-use chess::ChessBoard;
-use linera_sdk::views::{linera_views, RegisterView, RootView, ViewStorageContext};
+use chess::Game;
+use linera_sdk::{
+    base::Owner,
+    views::{linera_views, RegisterView, RootView, ViewStorageContext},
+};
 
 #[derive(RootView, async_graphql::SimpleObject)]
 #[view(context = "ViewStorageContext")]
 
 pub struct Chess {
-    pub board: RegisterView<ChessBoard>,
-}
-
-#[allow(dead_code)]
-impl Chess {
-    pub fn new(&mut self) {
-        let board: ChessBoard = ChessBoard::new();
-        self.board.set(board);
-    }
+    /// Players of the game
+    pub owners: RegisterView<Option<[Owner; 2]>>,
+    /// The current game state
+    pub board: RegisterView<Game>,
 }
