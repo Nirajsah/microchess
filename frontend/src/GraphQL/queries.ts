a9d524f74b089259cb93ba9a9cff7ba4a10c1bbe
Nirajsah <nirajsahy4aj@gmail.com>
2024-08-14 02:16:33 +0530

    Refactor GraphQL queries and add capture piece mutation

--- frontend/src/GraphQL/queries.ts
+++ frontend/src/GraphQL/queries.ts
@@ -30,16 +30,25 @@ export const MOVE_PIECE = gql`
     makeMove(from: $from, to: $to, piece: $piece)
   }
 `
+export const CAPTURE_PIECE = gql`
+  mutation CapturePiece(
+    $from: String!
+    $to: String!
+    $piece: String!
+    $captured: String!
+  ) {
+    capturePiece(from: $from, to: $to, piece: $piece, captured: $captured)
+  }
+`
+
 export const GET_PLAYER_TURN = gql`
   query {
     playerTurn
   }
 `
 
-export const GET_PLAYER_COLOR = gql`
-  query {
-    PlayerColor(player: Owner!) {
-      color
-    } 
+export const GET_PLAYER = gql`
+  query Player($player: ID!) {
+    player(player: $player)
   }
 `
