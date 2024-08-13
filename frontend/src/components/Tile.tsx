d98bf497ca36d96436838d8fd45068b974c3c417
Nirajsah <nirajsahy4aj@gmail.com>
2024-08-14 02:16:39 +0530

    Add Tile component for rendering chess board tiles

---
+++ frontend/src/components/Tile.tsx
@@ -0,0 +1,15 @@
+export default function Tile({ image }: { image: string }) {
+  return (
+    <div
+      style={{
+        width: '100px', // set your desired width
+        height: '100px', // set your desired height
+        backgroundImage: `url(${image})`,
+        backgroundSize: 'cover', // make sure the image covers the entire div
+        backgroundPosition: 'center', // center the image
+        backgroundRepeat: 'no-repeat', // prevent the image from repeating
+        cursor: 'pointer', // change the cursor to a grabbing hand
+      }}
+    ></div>
+  )
+}
