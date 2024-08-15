b02387d13558adbf7c009b11f4e0dd4eba5857df
Nirajsah <nirajsahy4aj@gmail.com>
2024-08-16 01:17:36 +0530

    Refactor tailwind.config.js to use TypeScript and add Montserrat font family

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -2,7 +2,11 @@
 export default {
   content: ['./src/**/*.{html,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        montserrat: ['Montserrat'],
+      },
+    },
   },
   plugins: [],
 }
