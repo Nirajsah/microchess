db26dc67c5f8db594c7a4c0d85d37d63c78f694e
Nirajsah <nirajsahy4aj@gmail.com>
2024-08-16 01:18:17 +0530

    Refactor App.tsx to use HomePage component and update font family

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,57 +1,9 @@
-import { useLazyQuery, useSubscription } from '@apollo/client'
-import { GET_BOARD, GET_PLAYER_TURN, NOTIFICATIONS } from './GraphQL/queries'
-import React from 'react'
-import CBoard from './components/CBoard'
+import HomePage from './components/HomePage'
 
 export default function App() {
-  const chainId = window.sessionStorage.getItem('chainId') ?? ''
-  const [player, setPlayer] = React.useState('')
-  const [boardState, setBoardState] = React.useState({})
-  const [boardQuery] = useLazyQuery(GET_BOARD, {
-    variables: {
-      endpoint: 'chess',
-      chainId: chainId,
-    },
-    onCompleted: (data) => {
-      console.log(data)
-
-      setBoardState(data.board)
-    },
-    fetchPolicy: 'network-only',
-  })
-
-  const [playerTurn, { called }] = useLazyQuery(GET_PLAYER_TURN, {
-    variables: {
-      endpoint: 'chess',
-      chainId: chainId,
-    },
-    onCompleted: (data) => {
-      console.log(data)
-      setPlayer(data.playerTurn)
-    },
-    fetchPolicy: 'network-only',
-  })
-
-  useSubscription(NOTIFICATIONS, {
-    variables: {
-      chainId: chainId,
-    },
-    onData: () => {
-      console.log('Notification received')
-      playerTurn()
-      boardQuery()
-    },
-  })
-
-  if (!called) {
-    playerTurn()
-    boardQuery()
-  }
-
   return (
-    <div className="p-5 flex justify-center items-center">
-      <div>Player to Move: {player}</div>
-      <CBoard boardState={boardState} active={player} />
+    <div className="w-full h-full font-montserrat">
+      <HomePage />
     </div>
   )
 }
