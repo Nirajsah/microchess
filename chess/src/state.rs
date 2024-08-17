9ccf293ad28ab1f1adeeb6746d0c3a280d4be215
Nirajsah <nirajsahy4aj@gmail.com>
2024-08-17 14:21:08 +0530

    Refactor Chess struct: Add game clock and players

--- chess/src/state.rs
+++ chess/src/state.rs
@@ -12,5 +12,34 @@ pub struct Chess {
     pub owners: MapView<Owner, Color>,
     /// The current game state
     pub board: RegisterView<Game>,
+    /// The current game clock
     pub clock: RegisterView<Clock>,
+    /// The current game players
+    pub players: RegisterView<Vec<Owner>>,
+}
+
+#[allow(dead_code)]
+impl Chess {
+    pub fn get_players(&self) -> &Vec<Owner> {
+        self.players.get()
+    }
+    pub fn add_player(&mut self, player: Owner) {
+        // let mut new_player = self.players.get();
+        // new_player.push(player);
+        self.players.get_mut().push(player);
+    }
+    pub fn opponent(&self, player: Owner) -> Option<Owner> {
+        log::info!("Player: {:?}", player);
+        let players = self.get_players();
+        if players.len() != 2 {
+            return None;
+        }
+        if players[0] == player {
+            log::info!("Player: {:?}", players[1]);
+            Some(players[1])
+        } else {
+            log::info!("Player: {:?}", players[0]);
+            Some(players[0])
+        }
+    }
 }
