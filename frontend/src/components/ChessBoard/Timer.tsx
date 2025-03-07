79afb70f594ee24944441d3b7de9ac024a779de1
Nirajsah <nirajsahy4aj@gmail.com>
2025-03-07 20:37:25 +0530

    feat: refactor Timer component to use updated props and improve time display format

--- frontend/src/components/ChessBoard/Timer.tsx
+++ frontend/src/components/ChessBoard/Timer.tsx
@@ -1,18 +1,37 @@
+import React, { useState, useEffect } from 'react'
+
 interface TimerProps {
-  initialTimeMs: number
-  start: boolean
+  initialTime: number
+  isActive: boolean
 }
 
-const Timer: React.FC<TimerProps> = ({ initialTimeMs, start }) => {
-  const totalSeconds = Math.floor(initialTimeMs / 1000000)
-  const minutes = Math.floor(totalSeconds / 60)
-  const seconds = totalSeconds % 60
+const Timer: React.FC<TimerProps> = ({ initialTime, isActive }) => {
+  const [time, setTime] = useState(initialTime)
 
-  console.log('Timer rendered', minutes, seconds, start)
+  useEffect(() => {
+    let interval: ReturnType<typeof setInterval> | null = null
+
+    if (isActive) {
+      interval = setInterval(() => {
+        setTime((prevTime) => prevTime - 1)
+      }, 1000)
+    }
+
+    return () => {
+      if (interval) {
+        clearInterval(interval)
+      }
+    }
+  }, [isActive])
+
+  const minutes = Math.floor(time / 60)
+  const seconds = time % 60
 
   return (
     <div>
-      <h1>{`${minutes}:${seconds.toString().padStart(2, '0')}`}</h1>
+      <h1>{`${minutes.toString().padStart(2, '0')}:${seconds
+        .toString()
+        .padStart(2, '0')}`}</h1>
     </div>
   )
 }
