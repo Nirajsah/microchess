a75c560b104a8f8f3eca89e58ca1f488f7048120
Nirajsah <nirajsahy4aj@gmail.com>
2025-03-20 18:59:31 +0530

    chore: updating drag and drop with mouseEvents for better functionality

--- frontend/src/components/ChessBoard/Tile.tsx
+++ frontend/src/components/ChessBoard/Tile.tsx
@@ -1,6 +1,5 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { Piece, Square, SquareToPieceMap } from './types'
-import generatePossibleMoves from './GeneratePossibleMoves'
 import { generate_possible_moves } from 'wasm'
 
 export default function Tile({
@@ -13,7 +12,9 @@ export default function Tile({
   whiteCastle,
   blackCastle,
   en_passant,
+  boardRef,
 }: {
+  boardRef: React.RefObject<HTMLDivElement>
   image: string | undefined
   piece: Piece
   square: Square
@@ -24,93 +25,91 @@ export default function Tile({
   blackCastle: boolean
   en_passant: string | null
 }) {
-  const tileRef = React.useRef<HTMLDivElement>(null)
+  const pieceRef = useRef<HTMLImageElement | null>(null)
+  const tileRef = useRef<HTMLImageElement | null>(null)
+  const [dragging, setDragging] = useState(false)
+  const [position, setPosition] = useState({ x: 0, y: 0 })
+  const offset = useRef({ x: 0, y: 0 })
 
-  const dragNdrop = parseInt(sessionStorage.getItem('dragNdrop') ?? '0', 10)
-  const isDraggable = Boolean(dragNdrop)
+  const handleMouseDown = (e: React.MouseEvent) => {
+    e.preventDefault()
+    setDragging(true)
 
-  const [isDragging, setIsDragging] = React.useState(false)
+    // Store offset relative to current position
+    offset.current = {
+      x: e.clientX - position.x,
+      y: e.clientY - position.y,
+    }
 
-  const onDragStart = (e: React.DragEvent<HTMLDivElement>) => {
-    setIsDragging(true)
-    e.dataTransfer.effectAllowed = 'move'
-    e.dataTransfer.setData('text/plain', `${piece}`)
     setSelectedSquare(square)
-    setTimeout(() => {
-      ;(e.target as HTMLElement).style.opacity = '0'
-    }, 0.0)
-    if (tileRef.current) {
-      tileRef.current.getBoundingClientRect()
-    }
-    try {
-      console.log('called inside try block')
-      console.log('board: ', board)
-      const possibleMoves = generate_possible_moves(
-        piece,
-        square,
-        board,
-        whiteCastle,
-        blackCastle,
-        en_passant as Square
-      )
-      console.log('Possible moves:', possibleMoves)
-      setPossMoves(possibleMoves as Square[])
-    } catch (err) {
-      console.error('Error generating possible moves:', err)
-    }
+
+    const mv = generate_possible_moves(
+      piece,
+      square,
+      board,
+      whiteCastle,
+      blackCastle,
+      en_passant as Piece
+    )
+    setPossMoves(mv as Square[])
   }
 
-  const onDragEnd = (e: React.DragEvent<HTMLDivElement>) => {
-    if (tileRef.current) {
-      tileRef.current.style.opacity = '1'
-    }
-    setTimeout(() => {
-      ;(e.target as HTMLElement).style.opacity = '1'
-    }, 10)
-    setIsDragging(false)
+  const handleMouseMove = (e: MouseEvent) => {
+    if (!dragging) return
+
+    setPosition({
+      x: e.clientX - offset.current.x,
+      y: e.clientY - offset.current.y,
+    })
   }
 
-  const handleDrag = (e: React.DragEvent<HTMLDivElement>) => {
-    if (tileRef.current && isDragging) {
-      e.preventDefault()
-      e.stopPropagation()
-    }
+  const handleMouseUp = () => {
+    setDragging(false)
+    const tileSize = tileRef.current?.getBoundingClientRect().width
+    if (!tileSize) return
+    setPosition((prev) => ({
+      x: Math.round(prev.x / tileSize) * tileSize,
+      y: Math.round(prev.y / tileSize) * tileSize,
+    }))
+    setPossMoves([])
+    setSelectedSquare(null)
   }
+
+  useEffect(() => {
+    if (dragging) {
+      document.addEventListener('mousemove', handleMouseMove)
+      document.addEventListener('mouseup', handleMouseUp)
+    } else {
+      document.removeEventListener('mousemove', handleMouseMove)
+      document.removeEventListener('mouseup', handleMouseUp)
+    }
+
+    return () => {
+      document.removeEventListener('mousemove', handleMouseMove)
+      document.removeEventListener('mouseup', handleMouseUp)
+    }
+  }, [dragging])
 
   return (
-    <div
-      ref={tileRef}
-      className={`flex items-center justify-center hover:scale-110 chess-piece transition-all duration-200 ${piece}`}
-      style={{
-        maxWidth: '50%',
-        maxHeight: '50%',
-        cursor: isDragging ? 'grabbing' : 'grab', // change the cursor to a grabbing hand
-        opacity: 1,
-      }}
-      draggable={isDraggable}
-      onDragStart={(e) => {
-        onDragStart(e)
-      }}
-      onDragEnd={(e) => {
-        onDragEnd(e)
-      }}
-      onDrag={handleDrag}
-    >
-      {image && (
-        <div className="w-full" draggable={isDraggable}>
-          <img
-            style={{
-              maxWidth: '100%', // Keep the image size consistent
-              maxHeight: '100%',
-              objectFit: 'contain',
-              opacity: 1,
-              backgroundColor: 'transparent',
-            }}
-            src={image}
-            alt={image}
-            draggable="false"
-          />
-        </div>
+    <div ref={tileRef} className="w-full h-full relative">
+      {piece && (
+        <img
+          ref={pieceRef}
+          src={image}
+          alt={piece}
+          style={{
+            width: '65px',
+            height: '65px',
+            position: 'absolute',
+            zIndex: 100,
+            left: `${position.x + 12}px`,
+            top: `${position.y + 12}px`,
+            cursor: dragging ? 'grabbing' : 'grab',
+            userSelect: 'none',
+          }}
+          draggable={false}
+          onMouseDown={handleMouseDown}
+        />
       )}
     </div>
   )
