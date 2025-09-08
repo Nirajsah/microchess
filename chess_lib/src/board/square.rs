3490ba8a8e7fd3fb7a0b6f151a5a63255c72cbf9
Nirajsah <nirajsahy4aj@gmail.com>
2025-09-08 16:36:46 +0530

    refactor: simplify Square::from_str and improve error handling

--- chess_lib/src/board/square.rs
+++ chess_lib/src/board/square.rs
@@ -39,35 +39,22 @@ impl Shr<Square> for u64 {
 impl FromStr for Square {
     type Err = String;
 
-#[rustfmt::skip]
-    /// Converts a string to a square.
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        match s {
-            "a1" => Ok(Square::A1), "b1" => Ok(Square::B1), "c1" => Ok(Square::C1), "d1" => Ok(Square::D1),
-            "e1" => Ok(Square::E1), "f1" => Ok(Square::F1), "g1" => Ok(Square::G1), "h1" => Ok(Square::H1),
-
-            "a2" => Ok(Square::A2), "b2" => Ok(Square::B2), "c2" => Ok(Square::C2), "d2" => Ok(Square::D2),
-            "e2" => Ok(Square::E2), "f2" => Ok(Square::F2), "g2" => Ok(Square::G2), "h2" => Ok(Square::H2),
-
-            "a3" => Ok(Square::A3), "b3" => Ok(Square::B3), "c3" => Ok(Square::C3), "d3" => Ok(Square::D3),
-            "e3" => Ok(Square::E3), "f3" => Ok(Square::F3), "g3" => Ok(Square::G3), "h3" => Ok(Square::H3),
-
-            "a4" => Ok(Square::A4), "b4" => Ok(Square::B4), "c4" => Ok(Square::C4), "d4" => Ok(Square::D4),
-            "e4" => Ok(Square::E4), "f4" => Ok(Square::F4), "g4" => Ok(Square::G4), "h4" => Ok(Square::H4),
-
-            "a5" => Ok(Square::A5), "b5" => Ok(Square::B5), "c5" => Ok(Square::C5), "d5" => Ok(Square::D5),
-            "e5" => Ok(Square::E5), "f5" => Ok(Square::F5), "g5" => Ok(Square::G5), "h5" => Ok(Square::H5),
-
-            "a6" => Ok(Square::A6), "b6" => Ok(Square::B6), "c6" => Ok(Square::C6), "d6" => Ok(Square::D6),
-            "e6" => Ok(Square::E6), "f6" => Ok(Square::F6), "g6" => Ok(Square::G6), "h6" => Ok(Square::H6),
-
-            "a7" => Ok(Square::A7), "b7" => Ok(Square::B7), "c7" => Ok(Square::C7), "d7" => Ok(Square::D7),
-            "e7" => Ok(Square::E7), "f7" => Ok(Square::F7), "g7" => Ok(Square::G7), "h7" => Ok(Square::H7),
-
-            "a8" => Ok(Square::A8), "b8" => Ok(Square::B8), "c8" => Ok(Square::C8), "d8" => Ok(Square::D8),
-            "e8" => Ok(Square::E8), "f8" => Ok(Square::F8), "g8" => Ok(Square::G8), "h8" => Ok(Square::H8),
-            _ => Err("Invalid square".to_string()),
+        if s.len() != 2 {
+            return Err("Invalid square length".to_string());
         }
+        let file = s.chars().next().unwrap();
+        let rank = s.chars().nth(1).unwrap();
+
+        if !('a'..='h').contains(&file) || !('1'..='8').contains(&rank) {
+            return Err("Invalid square".to_string());
+        }
+
+        let file_idx = (file as u8 - b'a') as u8;
+        let rank_idx = (rank as u8 - b'1') as u8;
+
+        let index = (rank_idx * 8 + file_idx) as usize;
+        Ok(Square::usize_to_square(index))
     }
 }
 
@@ -94,33 +81,95 @@ impl Square {
         ((*self as usize / 8) as u8) + 1
     }
 
-    #[rustfmt::skip]
     pub const fn usize_to_square(i: usize) -> Self {
-        match i {
-            0 => A1, 1 => B1, 2 => C1, 3 => D1, 4 => E1, 5 => F1, 6 => G1, 7 => H1,
-            8 => A2, 9 => B2, 10 => C2, 11 => D2, 12 => E2, 13 => F2, 14 => G2, 15 => H2,
-            16 => A3, 17 => B3, 18 => C3, 19 => D3, 20 => E3, 21 => F3, 22 => G3, 23 => H3,
-            24 => A4, 25 => B4, 26 => C4, 27 => D4, 28 => E4, 29 => F4, 30 => G4, 31 => H4,
-            32 => A5, 33 => B5, 34 => C5, 35 => D5, 36 => E5, 37 => F5, 38 => G5, 39 => H5,
-            40 => A6, 41 => B6, 42 => C6, 43 => D6, 44 => E6, 45 => F6, 46 => G6, 47 => H6,
-            48 => A7, 49 => B7, 50 => C7, 51 => D7, 52 => E7, 53 => F7, 54 => G7, 55 => H7,
-            56 => A8, 57 => B8, 58 => C8, 59 => D8, 60 => E8, 61 => F8, 62 => G8, 63 => H8,
-            _ => unreachable!(),
+        assert!(i < 64);
+        // SAFETY: `Square` is #[repr(u8)] and contiguous from 0..63
+        unsafe { std::mem::transmute(i as u8) }
+    }
+
+    pub fn usize_to_string(i: usize) -> String {
+        assert!(i < 64, "square index out of bounds: {}", i);
+
+        let file = (b'a' + (i % 8) as u8) as char;
+        let rank = (1 + (i / 8)) as u8;
+
+        format!("{}{}", file, rank)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::str::FromStr;
+
+    #[test]
+    fn test_shl_and_shr_with_square() {
+        let x: u64 = 1;
+        let sq = Square::E4; // index 28
+        assert_eq!(x << sq, 1u64 << 28);
+        assert_eq!(1u64 << 28 >> sq, x);
+    }
+
+    #[test]
+    fn test_from_str_valid() {
+        let sq = Square::from_str("a1").unwrap();
+        assert_eq!(sq, Square::A1);
+
+        let sq = Square::from_str("h8").unwrap();
+        assert_eq!(sq, Square::H8);
+
+        let sq = Square::from_str("e4").unwrap();
+        assert_eq!(sq, Square::E4);
+    }
+
+    #[test]
+    fn test_from_str_invalid() {
+        assert!(Square::from_str("i1").is_err());
+        assert!(Square::from_str("a9").is_err());
+        assert!(Square::from_str("a").is_err());
+        assert!(Square::from_str("").is_err());
+    }
+
+    #[test]
+    fn test_mirror() {
+        assert_eq!(Square::A1.mirror(), Square::A8);
+        assert_eq!(Square::H1.mirror(), Square::H8);
+        assert_eq!(Square::E4.mirror(), Square::E5);
+        assert_eq!(Square::D5.mirror(), Square::D4);
+    }
+
+    #[test]
+    fn test_rank() {
+        assert_eq!(Square::A1.rank(), 1);
+        assert_eq!(Square::H1.rank(), 1);
+        assert_eq!(Square::A8.rank(), 8);
+        assert_eq!(Square::E4.rank(), 4);
+        assert_eq!(Square::D5.rank(), 5);
+    }
+
+    #[test]
+    fn test_usize_to_square() {
+        for i in 0..64 {
+            let sq = Square::usize_to_square(i);
+            assert_eq!(sq as usize, i);
         }
     }
 
-    #[rustfmt::skip]
-    pub fn usize_to_string(i: usize) -> String {
-        match i {
-            0 => "a1".to_string(), 1 => "b1".to_string(), 2 => "c1".to_string(), 3 => "d1".to_string(), 4 => "e1".to_string(), 5 => "f1".to_string(), 6 => "g1".to_string(), 7 => "h1".to_string(),
-            8 => "a2".to_string(), 9 => "b2".to_string(), 10 => "c2".to_string(), 11 => "d2".to_string(), 12 => "e2".to_string(), 13 => "f2".to_string(), 14 => "g2".to_string(), 15 => "h2".to_string(),
-            16 => "a3".to_string(), 17 => "b3".to_string(), 18 => "c3".to_string(), 19 => "d3".to_string(), 20 => "e3".to_string(), 21 => "f3".to_string(), 22 => "g3".to_string(), 23 => "h3".to_string(),
-            24 => "a4".to_string(), 25 => "b4".to_string(), 26 => "c4".to_string(), 27 => "d4".to_string(), 28 => "e4".to_string(), 29 => "f4".to_string(), 30 => "g4".to_string(), 31 => "h4".to_string(),
-            32 => "a5".to_string(), 33 => "b5".to_string(), 34 => "c5".to_string(), 35 => "d5".to_string(), 36 => "e5".to_string(), 37 => "f5".to_string(), 38 => "g5".to_string(), 39 => "h5".to_string(),
-            40 => "a6".to_string(), 41 => "b6".to_string(), 42 => "c6".to_string(), 43 => "d6".to_string(), 44 => "e6".to_string(), 45 => "f6".to_string(), 46 => "g6".to_string(), 47 => "h6".to_string(),
-            48 => "a7".to_string(), 49 => "b7".to_string(), 50 => "c7".to_string(), 51 => "d7".to_string(), 52 => "e7".to_string(), 53 => "f7".to_string(), 54 => "g7".to_string(), 55 => "h7".to_string(),
-            56 => "a8".to_string(), 57 => "b8".to_string(), 58 => "c8".to_string(), 59 => "d8".to_string(), 60 => "e8".to_string(), 61 => "f8".to_string(), 62 => "g8".to_string(), 63 => "h8".to_string(),
-            _ => unreachable!(),
+    #[test]
+    fn test_usize_to_string() {
+        assert_eq!(Square::usize_to_string(0), "a1");
+        assert_eq!(Square::usize_to_string(7), "h1");
+        assert_eq!(Square::usize_to_string(8), "a2");
+        assert_eq!(Square::usize_to_string(63), "h8");
+        assert_eq!(Square::usize_to_string(27), "d4");
+    }
+
+    #[test]
+    fn test_shl_and_shr_consistency() {
+        for i in 0..64 {
+            let sq = Square::usize_to_square(i);
+            let x: u64 = 1;
+            assert_eq!(x << sq >> sq, x);
         }
     }
 }
